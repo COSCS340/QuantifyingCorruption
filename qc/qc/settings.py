985602623d385345ecf2ada3a0712676699a1d0c
Will Lifferth <wlifferth@icloud.com>
2018-03-21 14:12:22 -0400

    Added main/static/main to STATIC_DIRS

--- qc/qc/settings.py
+++ qc/qc/settings.py
@@ -120,3 +120,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static_media/')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    'main/static/main',
+)
