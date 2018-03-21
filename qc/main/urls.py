8a4f77dc90ab3f4a6c44cf4ed7639b99b8833444
Will Lifferth <wlifferth@icloud.com>
2018-03-21 13:03:35 -0400

    Created splash view function; closes #23

--- qc/main/urls.py
+++ qc/main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.helloworld, name = 'helloworld'),
+    path('hello/', views.helloworld, name='helloworld'),
+    path('', views.splash, name='splash'),
 ]
--- qc/main/views.py
+++ qc/main/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def helloworld(request):
     context = {}
     return render(request, 'main/hello.html', context)
+
+def splash(request):
+    context = {}
+    return HttpResponse("You made it to a splash page")
+    # return render(request, 'main/splash.html', context)
