10c6794edae4bfff9a2a8b2e51dd620a87b96136
Will Lifferth <wlifferth@icloud.com>
2018-03-21 11:55:03 -0400

    Created a sparse Legislator model

--- qc/main/models.py
+++ qc/main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Legislator(models.Model):
+    PARTY = [('D', 'Democrat'), ('R', 'Republican'), ('L', 'Libertarian'), ('G', 'Green Party'), ('I', 'Independent')]
+    name = models.CharField(max_length=100)
+    state = models.CharField(max_length=2)
+    party = models.CharField(max_length=2, choices=PARTY, default='I')
